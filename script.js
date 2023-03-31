b734e583d33cc27322bf23f1926bf39df0730dc2
Anton <Impl3x@yandex.ru>
2023-03-31 19:43:44 +0300

    hw11

--- script.js
+++ script.js
@@ -4,26 +4,32 @@ const nameInputElement = document.getElementById("name-input");
 const textInputElement = document.getElementById("text-input");
 const currentDate = new Date().toLocaleString();
 const deleteButton = document.getElementById("del-button");
-const commentElements = document.querySelectorAll(".comment");
+
+
+// Лайки и их счетчик
 
 const likeButtonsListeners = () => {
   const likeButtons = document.querySelectorAll(".like-button");
-// console.log(likeButtons);
+  // console.log(likeButtons);
   for (const likeButton of likeButtons) {
     likeButton.addEventListener('click', () => {
+      //  event.stopPropagation();
       const index = likeButton.dataset.like;
       if (comments[index].likedComment === "like-button") {
-        comments[index].likesComment +=1;
+        comments[index].likesComment += 1;
         comments[index].likedComment = "like-button -active-like";
-        
+
       } else {
-        comments[index].likesComment -=1;
-        comments[index].likedComment = "like-button";  
+        comments[index].likesComment -= 1;
+        comments[index].likedComment = "like-button";
       }
       renderComments();
     });
-  }  
+  }
 };
+
+
+//Данные о комментариях
 
 const comments = [{
     nameComment: "Глеб Фокин",
@@ -41,16 +47,19 @@ const comments = [{
   }
 ];
 
+
+//Рендер-функция
+
 const renderComments = () => {
   const commentsHtml = comments.map((comment, index) => {
-    return `<li class="comment" data-name="${comment.nameComment}">
+    return `<li class="comment" data-name="${protectHtml(comment.nameComment)}">
     <div class="comment-header">
-      <div>${comment.nameComment}</div>
+      <div class="comment-name">${protectHtml(comment.nameComment)}</div>
       <div>${comment.dateComment}</div>
     </div>
     <div class="comment-body">
-      <div class="comment-text">
-        ${comment.textComment}
+      <div class="comment-text" data-reply="${index}">
+        ${protectHtml(comment.textComment)}
       </div>
     </div>
     <div class="comment-footer">
@@ -63,70 +72,42 @@ const renderComments = () => {
   }).join('');
   listElement.innerHTML = commentsHtml;
   likeButtonsListeners();
-  
-  nameInputElement.addEventListener("input", inputCheck);
-  textInputElement.addEventListener("input", inputCheck);
-  nameInputElement.addEventListener("keyup", ({
-    key
-  }) => {
-    if (key === "Enter") {
-      nameInputElement.classList.remove("error");
-      if (nameInputElement.value === "") {
-        nameInputElement.classList.add("error");
-        return;
-      } else if (textInputElement.value === "") {
-        textInputElement.classList.add("error");
-        return;
-      }
-      renderComments();
-    }
-  
-  });
-  
-  textInputElement.addEventListener("keyup", ({
-    key
-  }) => {
-    if (key === "Enter") {
-      textInputElement.classList.remove("error");
-      if (textInputElement.value === "") {
-        textInputElement.classList.add("error");
-        return;
-      } else if (nameInputElement.value === "") {
-        nameInputElement.classList.add("error");
-        return;
-      }
-      renderComments();
-    }
-  
-  });
-  
+  buttonSwitch();
 };
-
 renderComments();
 
 
+//Новый элемент
+function addNewElement() {
+  buttonElement.addEventListener('click', () => {
+    nameInputElement.classList.remove("error");
+    if (nameInputElement.value === "") {
+      nameInputElement.classList.add("error");
+      return;
+    } else if (textInputElement.value === "") {
+      textInputElement.classList.add("error");
+      return;
+    }
+    comments.push({
+      nameComment: nameInputElement.value,
+      textComment: textInputElement.value,
+      dateComment: currentDate,
+      likesComment: 0,
+      likedComment: "like-button",
+    });
 
-buttonElement.addEventListener('click', () => {
-  nameInputElement.classList.remove("error");
-  if (nameInputElement.value === "") {
-    nameInputElement.classList.add("error");
-    return;
-  } else if (textInputElement.value === "") {
-    textInputElement.classList.add("error");
-    return;
-  }
-  comments.push({
-    nameComment: nameInputElement.value,
-    textComment: textInputElement.value,
-    dateComment: currentDate,
-    likesComment: 0,
-    likedComment: "like-button",
+    renderComments();
+    makeResponse();
   });
-  renderComments();
- 
-});
+}
+addNewElement();
 
-function inputCheck() {
+
+// Вкл/выкл кнопки добавления нового элемента в список
+nameInputElement.addEventListener("input", buttonSwitch);
+textInputElement.addEventListener("input", buttonSwitch);
+
+function buttonSwitch() {
   buttonElement.classList.remove("turn-off");
   if (document.getElementById("name-input").value === "" || document.getElementById("text-input").value === "") {
     document.getElementById("add-button").disabled = true;
@@ -136,13 +117,10 @@ function inputCheck() {
     buttonElement.classList.remove("turn-off");
   }
 }
+buttonSwitch();
 
 
-
-
-nameInputElement.addEventListener("input", inputCheck);
-
-textInputElement.addEventListener("input", inputCheck);
+// Работа по Enter
 
 nameInputElement.addEventListener("keyup", ({
   key
@@ -178,16 +156,29 @@ textInputElement.addEventListener("keyup", ({
 
 });
 
+//замена символов
+function protectHtml (anything = ""){
+  return anything.replaceAll("<","&lt;").replaceAll(">","&gt;");
+}
 
+protectHtml();
 
+// Ответ на комментарий
 
+const makeResponse = () => {
+
+  const allComments = document.querySelectorAll(".comment-text");
+  for (const oneComment of allComments) {
+    oneComment.addEventListener('click', () => {
+      const index = oneComment.dataset.reply;
+      textInputElement.value = "> " + comments[index].textComment + "\n" + comments[index].nameComment + ",";
+    });
+  }
+};
+
+makeResponse();
 
 //кнопка удаления
 deleteButton.addEventListener('click', () => {
   listElement.lastElementChild.remove(`${textInputElement.value}`);
 });
-
-
-
-
-
